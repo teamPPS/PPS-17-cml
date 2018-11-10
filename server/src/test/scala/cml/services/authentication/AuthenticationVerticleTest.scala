4b2057e84c7e4b989cdb2f1c945aa11d725568d7
Monica Gondolini <monica.gondolini@studio.unibo.it>
2018-11-10 17:48:34 +0100

    implemented some tests in AuthenticationTest.

--- server/src/test/scala/cml/services/authentication/AuthenticationVerticleTest.scala
+++ server/src/test/scala/cml/services/authentication/AuthenticationVerticleTest.scala
@@ -7,7 +7,8 @@ package cml.services.authentication
   */
 import cml.core.{BeforeAndAfterTest, HttpMessage, VerticleTesting}
 import cml.services.authentication.utils.AuthenticationConfig.AuthenticationUrl
-import io.vertx.core.Vertx
+import io.vertx.core.{Vertx, http}
+import io.vertx.scala.core.http._
 
 import scala.concurrent.Promise
 
@@ -30,4 +31,64 @@ class AuthenticationVerticleTest extends BeforeAndAfterTest {
     })
   }
 
+  test("Registration test"){
+    println("Registration test")
+    val vertx: Vertx = Vertx.vertx()
+    val promiseRegister = Promise[String]
+
+    val body: String = "{'username':'pps', 'password':'cml'}"
+
+    vertx.createHttpClient().post(8080, "127.0.0.1", AuthenticationUrl.REGISTER_API)
+      .putHeader("content-length", "1000")
+      .handler(response =>{
+          response.exceptionHandler(promiseRegister.failure)
+          response.bodyHandler(buffer => promiseRegister.success(buffer.toString))
+          response.statusCode()
+      }).write(body).end()
+
+    promiseRegister.future.map(res=>{
+      println("registration: " + res)
+      assert(res equals HttpMessage.BAD_REQUEST)
+    })
+  }
+
+  test("Login test"){
+    println("Login test")
+    val vertx: Vertx = Vertx.vertx()
+    val promiseLogin = Promise[String]
+    val body: String = "{'username':'pps', 'password':'cml'}"
+
+    vertx.createHttpClient().put(8080, "127.0.0.1", AuthenticationUrl.LOGIN_API)
+      .putHeader("content-length", "1000")
+      .handler(response =>{
+        response.exceptionHandler(promiseLogin.failure)
+        response.bodyHandler(buffer => promiseLogin.success(buffer.toString))
+        response.statusCode()
+      }).write(body).end()
+
+    promiseLogin.future.map(res=>{
+      println("login: " + res)
+      assert(res equals HttpMessage.BAD_REQUEST)
+    })
+  }
+
+//  test("Delete test"){
+//    println("Delete test")
+//    val vertx: Vertx = Vertx.vertx()
+//    val promiseDelete = Promise[String]
+//    val body: String = "{'username':'pps', 'password':'cml'}"
+//
+//    vertx.createHttpClient().delete(8080, "127.0.0.1", AuthenticationUrl.LOGIN_API)
+//      .putHeader("content-length", "1000")
+//      .handler(response =>{
+//        response.exceptionHandler(promiseDelete.failure)
+//        response.bodyHandler(buffer => promiseDelete.success(buffer.toString))
+//        response.statusCode()
+//      }).write(body).end()
+//
+//    promiseDelete.future.map(res=>{
+//      println("delete: " + res)
+//      assert(res equals HttpMessage.BAD_REQUEST)
+//    })
+//  }
 }
