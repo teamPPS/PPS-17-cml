d8710190d99aa0c839135be3a239e1df9fa11c48
ecavina <eugenio.cavina@studio.unibo.it>
2018-11-18 15:56:32 +0100

    refactoring GridInitializer following Style Class and fixed drag and drop with image

--- client/src/main/scala/cml/controller/fx/VillageViewController.scala
+++ client/src/main/scala/cml/controller/fx/VillageViewController.scala
@@ -4,7 +4,6 @@ import cml.utils.Configuration.{AuthenticationWindow, BattleWindow}
 import cml.view.{BaseGridInitializer, ViewSwitch}
 import javafx.fxml.FXML
 import javafx.scene.control._
-import javafx.scene.input._
 import javafx.scene.layout.GridPane
 
 class VillageViewController {
@@ -35,50 +34,4 @@ class VillageViewController {
     buildingsGrid setContent buildingsMenu
   }
 
-  // testing purpose
-  def initVillageMap(): Unit = {
-    // flyweight tile https://www.baeldung.com/java-flyweight https://refactoring.guru/design-patterns/flyweight/java/example
-
-    loop(0, 20) foreach {
-      case(x, y) =>
-        var mapLabel = new Label("terrain")
-        addDragAndDropTargetHandler(mapLabel)
-        villageMap add(mapLabel, x, y)
-    }
-
-    def loop(s: Int, e: Int) =
-      for(
-        x <- s until e;
-        y <- s until e
-      ) yield (x, y)
-  }
-
-
-  // https://examples.javacodegeeks.com/desktop-java/javafx/event-javafx/javafx-drag-drop-example/
-  def addDragAndDropSourceHandler(n: Label): Unit = { //al posto di Label ci andrà il tile personalizzato
-
-    n setOnDragDetected((event: MouseEvent) => {
-      val dragBoard: Dragboard = n startDragAndDrop TransferMode.COPY
-//      dragBoard setDragView(image) icona vicino mouse mentre si fa il drag
-      val content: ClipboardContent = new ClipboardContent
-      content putString(n getText)
-      dragBoard setContent content
-      event consume()
-    })
-
-  }
-
-  def addDragAndDropTargetHandler(n: Label): Unit = {
-
-    n setOnDragOver((event: DragEvent) => {
-      event acceptTransferModes TransferMode.COPY
-      event consume()
-    })
-
-    n setOnDragDropped((event: DragEvent) => {
-      var dragBoard: Dragboard = event getDragboard()
-      n setText dragBoard.getString
-      event consume()
-    })
-  }
 }
--- client/src/main/scala/cml/view/GridInitializer.scala
+++ client/src/main/scala/cml/view/GridInitializer.scala
@@ -1,24 +1,40 @@
 package cml.view
 
 import javafx.scene.SnapshotParameters
-import javafx.scene.control.Label
-import javafx.scene.image.Image
+import javafx.scene.image.{Image, ImageView}
 import javafx.scene.input._
 import javafx.scene.layout.GridPane
 
+/**
+  * Initialize Game GridPanes with style classes
+  */
 trait GridInitializer {
 
+  /**
+    * Initialize a grid as a buildings menu
+    * @param grid to initialize
+    */
   def initializeBuildingsMenu(grid: GridPane)
+
+  /**
+    * Initialize a grid as a village
+    * @param grid to initialize
+    */
   def initializeVillage(grid: GridPane)
 
 }
 
-object BaseGridInitializer extends GridInitializer {
+trait Setup {
 
+  def configure(grid: GridPane): GridPane
+
+}
+
+object Setup {
+
+  // LA PARTE DELLE TILE VA PORTATA FUORI, E' USATA IN PIU LUOGHI
   val spriteSheet: Image =  new Image(getClass.getClassLoader.getResource("image/Town64x64.png").toString, false)
-
   val slicer: ImageSlicer = new ImageSlicer(spriteSheet, 16, 16)
-
   val tileSet: Set[Tile] = Set[Tile](
     Tile("FARM", slicer.sliceAt(2, 3)),
     Tile("HABITAT", slicer.sliceAt(2, 3)),
@@ -26,67 +42,91 @@ object BaseGridInitializer extends GridInitializer {
     Tile("TERRAIN", slicer.sliceAt(5, 3))
   )
 
-  override def initializeBuildingsMenu(grid: GridPane): Unit = {
-    grid setHgap 10
-    grid setVgap 10
-    var pos = 0
-    for(tile <- tileSet) {
-      grid add(tile.imageSprite, 0, pos)
-      addDragAndDropSourceHandler(tile)
-      pos += 1
+
+  val setupVillage: Setup = {
+    grid: GridPane => { // scorrere model
+
+      //aggiungere handler per click sinistro per mostrare informazioni sulla gui
+
+      val baseTile = tileSet.filter(t => t.description.equals("TERRAIN")).head
+
+      loop(0, 10) foreach { // INVECE CHE GENERARE DA ZERO SCORRO IL MODEL E SETTO LE TILE CORRISPONDENTI
+        case(x, y) =>
+          val imageTile = new ImageView()
+          imageTile.setImage(baseTile.imageSprite.snapshot(new SnapshotParameters, null))
+          addDragAndDropTargetHandler(imageTile)
+          grid add(imageTile, x, y)
+      }
+
+      def loop(s: Int, e: Int) =
+      for(
+        x <- s until e;
+        y <- s until e
+      ) yield (x, y)
+
+      grid
     }
   }
 
-  override def initializeVillage(grid: GridPane): Unit = {
-    grid setHgap 10
-    grid setVgap 10
-    val baseTile = tileSet.filter(t => t.description.equals("TERRAIN")).head
-    loop(0, 10) foreach {
-      case(x, y) =>
-//        val terrainTile = Tile(baseTile.description, slicer.sliceAt(5, 3))
-        val terrainTile = new Label(baseTile.description)
-        addDragAndDropTargetHandler(terrainTile)
-        grid add(terrainTile, x, y)
+  val setupBuildingsMenu: Setup = {
+    grid: GridPane => {
+      var pos = 0
+      for(tile <- tileSet) {
+        grid add(tile.imageSprite, 0, pos)
+        addDragAndDropSourceHandler(tile)
+        pos += 1
+      }
+      grid
     }
   }
 
-  private def loop(s: Int, e: Int) =
-    for(
-      x <- s until e;
-      y <- s until e
-    ) yield (x, y)
-
-  // https://examples.javacodegeeks.com/desktop-java/javafx/event-javafx/javafx-drag-drop-example/
   def addDragAndDropSourceHandler(t: Tile): Unit = {
-
     val canvas = t.imageSprite
     canvas setOnDragDetected((event: MouseEvent) => {
       val dragBoard: Dragboard = canvas startDragAndDrop TransferMode.COPY
       val image = canvas.snapshot(new SnapshotParameters, null)
       dragBoard setDragView image
       val content: ClipboardContent = new ClipboardContent
-      content putImage image
       content putString t.description
       dragBoard setContent content
       event consume()
     })
-
   }
 
-  def addDragAndDropTargetHandler(t: Label): Unit = {
-
-    t setOnDragOver((event: DragEvent) => {
+  def addDragAndDropTargetHandler(c: ImageView): Unit = {
+    c setOnDragOver ((event: DragEvent) => {
       event acceptTransferModes TransferMode.COPY
       event consume()
     })
 
-    t setOnDragDropped((event: DragEvent) => {
+    c setOnDragDropped ((event: DragEvent) => {
       val dragBoard: Dragboard = event getDragboard()
-//      val newImage: Image = dragBoard.getImage
-//      image.getGraphicsContext2D.drawImage(newImage, 0, 0)
-      t setText dragBoard.getString
+      val newTile = tileSet.filter(t => t.description.equals(dragBoard.getString)).head
+      c setImage newTile.imageSprite.snapshot(new SnapshotParameters, null)
+      val y = GridPane.getColumnIndex(c)
+      val x = GridPane.getRowIndex(c)
+      println("Dropped element " + dragBoard.getString + " in coordinates (" + x + " - " + y + ")")
       event consume()
+      // UPDATE MODEL senza(???) modificare la GUI e send al server
+      // se ??? TODO aggiungere engine che renderizza il model abbastanza rapido da fare sembrare all'utente che droppando aggiunge l'elemento
     })
+  }
+}
+
+object BaseGridInitializer extends GridInitializer {
+
+  private def setupGrid(grid: GridPane, setup: Setup): Unit = {
+    grid setHgap 10
+    grid setVgap 10
+    setup.configure(grid)
+  }
+
+  override def initializeBuildingsMenu(grid: GridPane): Unit = {
+    setupGrid(grid, Setup.setupBuildingsMenu)
+  }
+
+  override def initializeVillage(grid: GridPane): Unit = {
+    setupGrid(grid, Setup.setupVillage)
   }
 
 }
