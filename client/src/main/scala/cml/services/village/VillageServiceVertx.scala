4fbcdcb5b2c91199fe06e329ee8534647e63995f
Monica Gondolini <monica.gondolini@studio.unibo.it>
2018-11-21 23:47:50 +0100

    added doc to VillageServiceVertx.

--- client/src/main/scala/cml/services/village/VillageServiceVertx.scala
+++ client/src/main/scala/cml/services/village/VillageServiceVertx.scala
@@ -1,16 +1,30 @@
 package cml.services.village
 
 import akka.actor.ActorRef
+import io.vertx.scala.core.Vertx
+import io.vertx.scala.ext.web.client.WebClient
 
+
+/**
+  * This trait describes the Village Vertx client
+  * @author Monica Gondolini
+  */
 trait VillageServiceVertx {
 
 }
 
 object VillageServiceVertx{
 
-  def apply(actor: ActorRef): VillageServiceVertx = new VillageServiceVertxImpl(actor)
+  val vertx: Vertx = Vertx.vertx()
+  var client: WebClient = WebClient.create(vertx)
 
-  case class VillageServiceVertxImpl(ref: ActorRef) extends VillageServiceVertx{
-    
+  def apply(actor: ActorRef): VillageServiceVertx = VillageServiceVertxImpl(actor)
+
+  /**
+    * This class implements the Village Vertx Client
+    * @param actor
+    */
+  case class VillageServiceVertxImpl(actor: ActorRef) extends VillageServiceVertx{
+
   }
 }
