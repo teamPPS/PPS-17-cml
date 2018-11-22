791541db03a031d024a8a445a9192dfe2ba749d4
Monica Gondolini <monica.gondolini@studio.unibo.it>
2018-11-22 17:31:33 +0100

    edited VillageActor.

--- client/src/main/scala/cml/controller/VillageActor.scala
+++ client/src/main/scala/cml/controller/VillageActor.scala
@@ -6,6 +6,8 @@ import cml.controller.messages.VillageRequest.{CreateVillage, DeleteVillage, Ent
 import cml.services.village.VillageServiceVertx
 import cml.services.village.VillageServiceVertx.VillageServiceVertxImpl
 
+import scala.util.{Failure, Success}
+
 
 /**
   * Class that implements the actor which manages the village interaction with user
@@ -21,8 +23,24 @@ class VillageActor() extends Actor{ //controller: VillageViewController
     */
   override def receive: Receive = {
     case CreateVillage(username) => villageVertx.createVillage(username)
+        .onComplete{
+          case Success(httpResponse) => ???
+          case Failure(exception) => ???
+        }
     case EnterVillage(username) => villageVertx.enterVillage(username)
+      .onComplete{
+        case Success(httpResponse) => ???
+        case Failure(exception) => ???
+      }
     case UpdateVillage(username, update) => villageVertx.updateVillage(username,update)
+      .onComplete{
+        case Success(httpResponse) => ???
+        case Failure(exception) => ???
+      }
     case DeleteVillage(username) => villageVertx.deleteVillageAndUser(username)
+      .onComplete{
+        case Success(httpResponse) => ???
+        case Failure(exception) => ???
+      }
   }
 }
