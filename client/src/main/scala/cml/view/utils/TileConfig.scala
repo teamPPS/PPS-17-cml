6ef781974babb5898b8919fb1017863c2973b4f2
Monica Gondolini <monica.gondolini@studio.unibo.it>
2018-11-30 16:01:14 +0100

    added json field to TileConfig.

--- client/src/main/scala/cml/view/utils/TileConfig.scala
+++ client/src/main/scala/cml/view/utils/TileConfig.scala
@@ -1,17 +1,29 @@
 package cml.view.utils
 
 import cml.view.{ImageSlicer, Tile}
+import cml.schema.Village._
 import javafx.scene.image.Image
+import play.api.libs.json.{JsValue, Json}
+
 
 object TileConfig {
 
   val spriteSheet: Image =  new Image(getClass.getClassLoader.getResource("image/Town64x64.png").toString, false)
   val slicer: ImageSlicer = new ImageSlicer(spriteSheet, 16, 16)
   val tileSet: Set[Tile] = Set[Tile](
-    Tile("FARM", slicer.sliceAt(2, 3)),
+    Tile("FARM", slicer.sliceAt(2, 3), buildingJson("Farm")),
     Tile("HABITAT", slicer.sliceAt(2, 3)),
-    Tile("CAVE", slicer.sliceAt(2, 3)),
-    Tile("TERRAIN", slicer.sliceAt(5, 3))
+    Tile("CAVE", slicer.sliceAt(2, 3), buildingJson("Cave")),
+    Tile("TERRAIN", slicer.sliceAt(5, 3), buildingJson("Farm"))
   )
 
+  def buildingJson(bType: String): JsValue = {
+    val json: JsValue = Json.obj(
+      "building_id" -> Json.obj(
+        BUILDING_TYPE -> bType,
+        BUILDING_LEVEL -> 1
+      )
+    )
+    json
+  }
 }
