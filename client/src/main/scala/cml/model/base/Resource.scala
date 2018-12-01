14dff475f077e9b225e0f87788de9a4837f7e503
Monica Gondolini <monica.gondolini@studio.unibo.it>
2018-12-01 17:13:30 +0100

    added decrement method to Resource model.

--- client/src/main/scala/cml/model/base/Resource.scala
+++ client/src/main/scala/cml/model/base/Resource.scala
@@ -20,6 +20,11 @@ trait Resource{
   def take(): Int
 
   /**
+    * Decrements the resource
+    */
+  def dec(price: Int): Unit
+
+  /**
     * The amount of resources
     */
   def amount: Int
@@ -38,7 +43,9 @@ case class Money(var moneyAmount: Int) extends Resource {
     moneyAmount = INIT_VALUE
     taken
   }
+  override def dec(price: Int): Unit = moneyAmount -= price
   override def amount: Int = moneyAmount
+
 }
 
 /**
@@ -54,6 +61,7 @@ case class Food(var foodAmount: Int) extends Resource {
     foodAmount = INIT_VALUE
     taken
   }
+  override def dec(price: Int): Unit = foodAmount -= price
   override def amount: Int = foodAmount
 }
 
