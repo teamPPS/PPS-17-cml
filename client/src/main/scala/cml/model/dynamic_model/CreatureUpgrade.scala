98e77d1405d499a9cee21adbcfb7269753b79f35
Monica Gondolini <monica.gondolini@studio.unibo.it>
2018-12-02 15:35:58 +0100

    added some doc to dynamic models.

--- client/src/main/scala/cml/model/dynamic_model/CreatureUpgrade.scala
+++ client/src/main/scala/cml/model/dynamic_model/CreatureUpgrade.scala
@@ -4,10 +4,18 @@ import cml.model.base.Creature
 import cml.utils.CreatureJson
 import play.api.libs.json.JsValue
 
+/**
+  * @author Monica Gondolini
+  */
+
 trait UpgradeCreature {
   def creatureJson: JsValue
 }
 
+/**
+  * Upgrade a creature level
+  * @param creature creature to model
+  */
 case class CreatureUpgrade(creature: Creature) extends UpgradeCreature {
   creature.levelUp()
   val json: JsValue = CreatureJson("Smaug", creature.currentLevel).json
