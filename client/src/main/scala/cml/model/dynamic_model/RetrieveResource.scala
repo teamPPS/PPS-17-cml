383df4772a3356a00a551e99b80fbd285c5d0a13
Monica Gondolini <monica.gondolini@studio.unibo.it>
2018-12-02 18:25:10 +0100

    added Retrieve Resource class.

---
+++ client/src/main/scala/cml/model/dynamic_model/RetrieveResource.scala
@@ -0,0 +1,28 @@
+package cml.model.dynamic_model
+
+import cml.model.base.Structure
+import cml.utils.ModelConfig.ModelClass.{CAVE, FARM, HABITAT}
+import cml.utils.{FoodJson, MoneyJson}
+import play.api.libs.json.JsValue
+
+
+/**
+  * @author Monica Gondolini
+  */
+trait Retrieve {
+  def resourceJson: JsValue
+}
+
+case class RetrieveResource(s: Structure) extends Retrieve {
+
+  var json: JsValue = _
+
+  s.resource.take()
+  s.getClass.getName match {
+    case FARM => json = FoodJson(s.resource amount).json
+    case CAVE =>  json = MoneyJson(s.resource amount).json
+    case HABITAT => json = MoneyJson(s.resource amount).json
+  }
+
+  override def resourceJson: JsValue = json
+}
