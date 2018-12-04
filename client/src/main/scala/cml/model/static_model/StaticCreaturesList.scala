c6116c9b213144cc3cad4dc9f4a6f5110b7f2f35
Monica Gondolini <monica.gondolini@studio.unibo.it>
2018-12-04 17:16:47 +0100

    added creatures to staticcreaturesList.

--- client/src/main/scala/cml/model/static_model/StaticCreaturesList.scala
+++ client/src/main/scala/cml/model/static_model/StaticCreaturesList.scala
@@ -1,8 +1,8 @@
 package cml.model.static_model
 
 import cml.model.base.Creature
-import cml.model.creatures.{Dragon, Golem}
-import cml.utils.ModelConfig.Creature.{DRAGON_NAME, GOLEM_NAME, INITIAL_LEVEL}
+import cml.model.creatures.{Dragon, Golem, Griffin, Kraken}
+import cml.utils.ModelConfig.Creature._
 
 /**
   * This object defines some static creatures
@@ -13,7 +13,9 @@ object StaticCreaturesList {
 
   val creaturesList : List[Creature] = List(
     Dragon(DRAGON_NAME,INITIAL_LEVEL),
-    Golem(GOLEM_NAME,INITIAL_LEVEL)
+    Golem(GOLEM_NAME,INITIAL_LEVEL),
+    Griffin(GRIFFIN_NAME, INITIAL_LEVEL),
+    Kraken(KRAKEN_NAME,INITIAL_LEVEL)
   )
 
 }
