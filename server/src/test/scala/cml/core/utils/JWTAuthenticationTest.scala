06f1b048c209b7caf6a6d997fdd0adb365c855f9
MrFilo92 <>
2018-12-04 17:13:09 +0100

    Refactoring.

--- server/src/test/scala/cml/core/utils/JWTAuthenticationTest.scala
+++ server/src/test/scala/cml/core/utils/JWTAuthenticationTest.scala
@@ -12,11 +12,11 @@ class JWTAuthenticationTest extends FunSuite {
   val username1 : String = "Filippo"
   val username2 : String = "Luca"
   val token1 : Option[String] = JWTAuthentication.encodeUsernameToken(username1)
-  val token2 : Option[String] = JWTAuthentication.encodeUsernameToken(username1)
+  val token2 : Option[String] = JWTAuthentication.encodeUsernameToken(username2)
 
   test("test to verify that an encoded token is different from another one"){
 
-    assert(JWTAuthentication.encodeUsernameToken(username1) != JWTAuthentication.encodeUsernameToken(username2))
+    assert(token1 != token2)
   }
 
   test("test to verify that an encoded token is equal to another one"){
