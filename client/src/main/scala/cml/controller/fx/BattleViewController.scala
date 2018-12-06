768f33eb4e5dfdaf8fec33847bd5369c4ea24d6d
Monica Gondolini <monica.gondolini@studio.unibo.it>
2018-12-06 14:31:01 +0100

    started initialization of listview.

--- client/src/main/scala/cml/controller/fx/BattleViewController.scala
+++ client/src/main/scala/cml/controller/fx/BattleViewController.scala
@@ -10,8 +10,11 @@ import akka.actor.{ActorSystem, Props}
 import cml.model.base.{Creature, VillageMap}
 import javafx.fxml.FXML
 import javafx.scene.control.Alert.AlertType
-import javafx.scene.control.{Alert, Button, ButtonType}
+import javafx.scene.control._
 import com.typesafe.config.ConfigFactory
+import javafx.collections.{FXCollections, ObservableList}
+import javafx.scene.image.ImageView
+import javafx.scene.layout.Pane
 
 import scala.collection.mutable
 
@@ -27,19 +30,30 @@ class BattleViewController {
   Locale.setDefault(Locale.ENGLISH)
 
   @FXML var exitButton: Button = _
+  @FXML var creatureViewPane: Pane = _
+  @FXML var creatureList: ListView[Creature] = _
+  @FXML var creatureImage: ImageView = _
+  @FXML var creatureSelectionPane: Pane = _
+  @FXML var creatureArea: TextArea = _
+  @FXML var creatureButton: Button = _
 
   val village: VillageMap = VillageMap.village
   var creatures: mutable.MutableList[Creature] = _
+  var obs: ObservableList[Creature] = FXCollections.observableArrayList()
 
   def initialize(): Unit = {
     println("battle view")
-    println(village.structures)
     for(s <- village.structures){
       if(s.creatures != null && s.creatures.nonEmpty){
        creatures = s.creatures
-        println(s.creatures)
+        obs add creatures.head
+        println(obs)
       }
     }
+    creatureList = new ListView[Creature](obs)
+//    creatureList.setCellFactory(_ => new ListCell[Creature](){
+//        override protected updateItem()
+//    })
   }
 
   @FXML
