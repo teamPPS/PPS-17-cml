46515a54f841bd1d77991f99a4bbd4a08fe292b6
ecavina <eugenio.cavina@studio.unibo.it>
2018-12-15 16:49:08 +0100

    added new way to populate villagemap model with strategy

--- client/src/main/scala/cml/model/base/VillageMap.scala
+++ client/src/main/scala/cml/model/base/VillageMap.scala
@@ -2,25 +2,34 @@ package cml.model.base
 
 import scala.collection.mutable
 
+trait VillageMap[A] {
+
+  def initVillage(structures: mutable.MutableList[Structure], gold: Int, food: Int, villageName: String): Unit
+  def createVillage[A](howToPopulate: A => Unit)(villageInfo: A): Unit
+  def instance(): Option[ConcreteVillageMap]
+}
+
 /**
   * Case class representing the current state of the user's village
   * @author ecavina, Monica Gondolini
-  * @param villageStructure
-  * @param gold
-  * @param food
-  * @param username
+  * @param villageStructure list of structures
+  * @param gold village's gold
+  * @param food village's foog
+  * @param villageName owner of the village
   */
-case class VillageMap (villageStructure: mutable.MutableList[Structure], var gold: Int, var food: Int, var username: String)
+case class ConcreteVillageMap (villageStructure: mutable.MutableList[Structure], var gold: Int, var food: Int, var villageName: String)
 
 object VillageMap {
 
-  private var _instance : Option[VillageMap] = Option.empty
+  private var _instance : Option[ConcreteVillageMap] = Option.empty
 
-  def initVillage(structures: mutable.MutableList[Structure], gold: Int, food: Int, user: String): Unit = {
-    _instance = Option(VillageMap(structures, gold, food, user))
+  def initVillage(structures: mutable.MutableList[Structure], gold: Int, food: Int, villageName: String): Unit = {
+    _instance = Option(VillageMap(structures, gold, food, villageName))
   }
 
-  def instance(): Option[VillageMap] = {
+  def createVillage[A](howToPopulate: A => Unit)(villageInfo: A): Unit = howToPopulate(villageInfo)
+
+  def instance(): Option[ConcreteVillageMap] = {
     if (_instance.isEmpty)
       initVillage(mutable.MutableList[Structure](), 0, 0, "default")
     _instance
